914ceb1c5048b38a1d5d519d2ba095be32855122
Chi Duoc SE <chiduocse@gmail.com>
2017-09-07 10:20:54 +0700

    lesson16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import{RouterModule} from '@angular/router';
-
 import { AppComponent } from './app.component';
 import{appRoutes} from './app.routes';
 
@@ -11,6 +12,8 @@ import{appRoutes} from './app.routes';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { Routes, RouterModule } from '@angular/router';
+import { NotificationService } from '../core/services/notification.service';
+import { AuthenService } from '../core/services/authen.service';
+import{FormsModule} from '@angular/forms'; 
 export const routes: Routes = [
   { path: '', component: LoginComponent }
 ]
@@ -9,8 +12,10 @@ export const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ],
+  providers:[AuthenService, NotificationService],
   declarations: [LoginComponent]
 })
 export class LoginModule { }
