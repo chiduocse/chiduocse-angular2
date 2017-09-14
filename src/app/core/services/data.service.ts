4595d1c5448e60e66b0183f5bdd4a5f4d4095d82
Chi Duoc SE <chiduocse@gmail.com>
2017-09-14 23:47:51 +0700

    lesson 21

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -5,17 +5,18 @@ import { SystemConstants } from './../common/system.constants';
 import { AuthenService } from './authen.service';
 import { NotificationService } from './notification.service';
 import { UtilityService } from './utility.service';
-import { Observable } from 'rxjs/Observable';
-import { MessageConstants } from './../common/message.constants';
+import {Observable} from 'rxjs/Observable';
+import {MessageConstants} from './../common/message.constants';
 
 @Injectable()
 export class DataService {
   private headers: Headers;
   constructor(private _http: Http, private _router: Router, private _authenService: AuthenService,
-    private _notificationService: NotificationService, private _utilityService: UtilityService) {
-    this.headers = new Headers();
-    this.headers.append('Content-Type', 'application/json');
-  }
+  private _notificationService: NotificationService,private _utilityService : UtilityService) {
+    this.headers  = new Headers();
+    this.headers.append('Content-Type','application/json');
+   }
+
   get(uri: string) {
     this.headers.delete("Authorization");
     this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
@@ -23,44 +24,43 @@ export class DataService {
   }
   post(uri: string, data?: any) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
   }
   put(uri: string, data?: any) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.put(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
   }
-  dalete(uri: string, key: string, id: string) {
+  delete(uri: string, key: string, id: string) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.delete(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers })
       .map(this.extractData);
   }
   postFile(uri: string, data?: any) {
-    let newHeader = new Headers;
-    newHeader.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
-    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers })
+    let newHeader = new Headers();
+    newHeader.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: newHeader })
       .map(this.extractData);
   }
   private extractData(res: Response) {
     let body = res.json();
     return body || {};
   }
+   public handleError(error: any) {
+        if (error.status == 401) {
+            localStorage.removeItem(SystemConstants.CURRENT_USER);
+            this._notificationService.printErrorMessage(MessageConstants.LOGIN_AGAIN_MSG);
+            this._utilityService.navigateToLogin();
+        }
+        else {
+            let errMsg = (error.message) ? error.message :
+                error.status ? `${error.status} - ${error.statusText}` : 'Lỗi hệ thống';
+            this._notificationService.printErrorMessage(errMsg);
 
-  public handleError(error: any) {
-    if (error.status == 401) {
-      localStorage.removeItem(SystemConstants.CURRENT_USER);
-      this._notificationService.printErrorMessage(MessageConstants.LOGIN_AGAIN_MSG);
-      this._utilityService.navigateToLogin();
+            return Observable.throw(errMsg);
+        }
+
     }
-    else {
-      let errMsg = (error.message) ? error.message :
-        error.status ? `${error.status} - ${error.statusText}` : 'Lỗi hệ thống';
-      this._notificationService.printErrorMessage(errMsg);
-
-      return Observable.throw(errMsg);
-    }
-
-  }
 }
