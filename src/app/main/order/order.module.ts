91ff237c23928e022cb8f4ac3332b35f2de1f804
Chi Duoc SE <chiduocse@gmail.com>
2017-10-15 16:19:48 +0700

    lesson38

---
+++ src/app/main/order/order.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginationModule, ModalModule } from 'ngx-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { DataService } from './../../core/services/data.service';
+import { UtilityService } from './../../core/services/utility.service';
+import { UploadService } from './../../core/services/upload.service';
+import { Daterangepicker } from 'ng2-daterangepicker';
+import { OrderRouter } from './order-routes';
+import { OrderComponent } from './order.component';
+import { OrderAddComponent } from './order-add/order-add.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    PaginationModule,
+    Daterangepicker,
+    ModalModule,
+    OrderRouter
+  ],
+  declarations: [OrderComponent, OrderAddComponent, OrderDetailComponent],
+  providers: [DataService, UtilityService, UploadService]
+})
+export class OrderModule { }
